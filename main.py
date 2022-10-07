d2fe45b3ed076c7a80485aaa9ed299c63b8a55ee
Ks Krimi <ny.kalash@gmail.com>
2022-10-07 15:08:44 +0200

    feat: stocke les données post dans une variable data

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Optional
+from uuid import UUID, uuid4
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+data = []
+
 
 class Post(BaseModel):
+    id: Optional[UUID] = uuid4()
     title: str
     content: str
     published: bool = True
@@ -20,9 +25,11 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return data
 
 
 @app.post("/posts")
 def create_post(post: Post):
-    return post.dict()
+    post.id = uuid4()  # generate new uuid
+    data.append(post)
+    return post
