80b10bcd9a40868b9587417a84186d0022acb26e
Ks Krimi <ny.kalash@gmail.com>
2022-10-10 18:52:00 +0200

    feat: crée un schema pour user

--- schemas.py
+++ schemas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class PostBase(BaseModel):
@@ -23,6 +23,25 @@ class PostUpdate(PostBase):
 class Post(PostBase):
     id: int
     created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+# there are a schema used for user's endpoints
+
+class UserBase(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class User(BaseModel):
+    id: int
+    email: EmailStr
 
     class Config:
         orm_mode = True
