2bb4ad4b4a9cee96904d91385866c3e4e7719bbe
Ks Krimi <ny.kalash@gmail.com>
2022-10-10 18:51:04 +0200

    feat: crée un model sqlalchemy pour user

--- model.py
+++ model.py
@@ -15,3 +15,15 @@ class Post(Base):
         TIMESTAMP(timezone=True),
         server_default=text("(DATETIME('now'))")
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default=text("(DATETIME('now'))")
+    )
