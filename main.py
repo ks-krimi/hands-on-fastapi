3b231a021cffd9a0d603a0a8aba6f51f7dc1b3d8
Ks Krimi <ny.kalash@gmail.com>
2022-10-10 21:23:17 +0200

    style: réorganise l'ordre de la ligne de code

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(post.router)
-app.include_router(user.router)
-
 
 @app.get("/", tags=["Root"])
 def root():
     return {"hello": "world"}
+
+
+app.include_router(post.router)
+app.include_router(user.router)
