43c18b29a3d380e4a8eae1e5dd50bdb27dddaa57
Ks Krimi <ny.kalash@gmail.com>
2022-10-11 09:20:27 +0200

    feat: crée une fonction pour vérifier le mot de passe d'un utilisateur pendant son login

--- utils.py
+++ utils.py
@@ -5,3 +5,7 @@ pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 def hash(pwd: str):
     return pwd_context.hash(pwd)
+
+
+def verify(pwd: str, hased_pwd: str):
+    return pwd_context.verify(pwd, hased_pwd)
