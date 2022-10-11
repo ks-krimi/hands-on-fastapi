5a68308e67627cd876d4e1b9d9994a607e95f6e8
Ks Krimi <ny.kalash@gmail.com>
2022-10-11 10:17:03 +0200

    feat: ajout du route login dans nos endpoints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from database import Base, engine
-from routers import post, user
+from routers import auth, post, user
 
 Base.metadata.create_all(bind=engine)
 
@@ -16,3 +16,4 @@ def root():
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
